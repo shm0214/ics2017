d983fdcba9011a8d1500254a0f207394a88952e7
tracer-ics2017 <tracer@njuics.org>
2022-03-04 15:26:36 +0800

    >  compile 1911463 shm0214 Linux shm0214 4.15.0-169-generic #177-Ubuntu SMP Thu Feb 3 10:50:46 UTC 2022 i686 i686 i686 GNU/Linux  15:26:36 up  3:33,  1 user,  load average: 0.57, 0.28, 0.17 484a7648920a30e2b72219bd3aaadaa4e063ce2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -276,40 +276,42 @@ uint32_t eval(int p, int q, bool* success) {
       *success = false;
       return 0;
     }
-    if (tokens[op].type == TK_MINUS && *success == false) {
+    if (*success == false && (tokens[op].type == TK_MINUS || get_priority(tokens[op].type == 5))) {
       *success = true;
-      int num = 0;
-      for (int i = op; i >= p; i--){
-        if (tokens[i].type == TK_MINUS)
-          num++;
+      if (tokens[op].type == TK_MINUS) {
+        int num = 0;
+        for (int i = op; i >= p; i--){
+          if (tokens[i].type == TK_MINUS)
+            num++;
+          else
+            break;
+        }
+        if (num % 2 != 0)
+          val2 = -val2;
+        if (op - num <= p) {
+          *success = true;
+          return val2;
+        }
+        if (p <= op - num - 1)
+          val1 = eval(p, op - num - 1, success);
         else
-          break;
-      }
-      if (num % 2 != 0)
-        val2 = -val2;
-      if (op - num <= p) {
-        *success = true;
-        return val2;
-      }
-      if (p <= op - num - 1)
-        val1 = eval(p, op - num - 1, success);
-      else
-        val1 = 0;
-      switch (tokens[op - num].type) {
-        case TK_PLUS:
-          return val1 + val2;
-        case TK_MINUS:
-          return val1 - val2;
-        case TK_MULTIPLY:
-          return val1 * val2;
-        case TK_DIV:
-          if (val2 == 0) {
-            *success = false;
-            return 0;
-          }
-          return val1 / val2;
-        default:
-          assert(0);
+          val1 = 0;
+        switch (tokens[op - num].type) {
+          case TK_PLUS:
+            return val1 + val2;
+          case TK_MINUS:
+            return val1 - val2;
+          case TK_MULTIPLY:
+            return val1 * val2;
+          case TK_DIV:
+            if (val2 == 0) {
+              *success = false;
+              return 0;
+            }
+            return val1 / val2;
+          default:
+            assert(0);
+        }
       }
     } else {
       switch (tokens[op].type) {
