a98e407cbc14f0a63c996f59875733bf526eff45
tracer-ics2017 <tracer@njuics.org>
2022-03-06 10:10:02 +0800

    >  compile 1911463 shm0214 Linux shm0214 4.15.0-169-generic #177-Ubuntu SMP Thu Feb 3 10:50:46 UTC 2022 i686 i686 i686 GNU/Linux  10:10:02 up 1 day,  1:54,  1 user,  load average: 0.82, 0.47, 0.29 7211e78ff4a8949485309248e81e653cc2c5d0

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,9 +1,13 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov);
-make_EHelper(call);
 
 make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+make_EHelper(call);
+make_EHelper(push);
+make_EHelper(pop);
+
