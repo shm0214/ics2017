8e596fa877376a1a802eaa3385897fd645375499
tracer-ics2017 <tracer@njuics.org>
2022-03-10 01:13:19 +0800

    >  compile 1911463 shm0214 Linux shm0214 4.15.0-169-generic #177-Ubuntu SMP Thu Feb 3 10:50:46 UTC 2022 i686 i686 i686 GNU/Linux  01:13:19 up  5:41,  1 user,  load average: 0.36, 0.21, 0.41 65b5d72a2c46623bfb75c50cb42bb172a2a20f75

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,7 +6,9 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  rtl_push(&id_dest->val);
+  // 没查到为啥要符号扩展，但事实如此
+  rtl_sext(&t0, &id_dest->val, id_dest->width);
+  rtl_push(&t0);
   print_asm_template1(push);
 }
 
