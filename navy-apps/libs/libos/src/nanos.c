c16362890ada60704cdae4812a6209ab7341b5b8
tracer-ics2017 <tracer@njuics.org>
2022-03-23 22:13:44 +0800

    >  run 1911463 shm0214 Linux shm0214 4.15.0-171-generic #180-Ubuntu SMP Wed Mar 2 17:24:46 UTC 2022 i686 i686 i686 GNU/Linux  22:13:44 up  7:49,  1 user,  load average: 2.28, 1.38, 0.95 767c00f76f18deb7a0c93de31320d91f6bcf5a5a

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -32,12 +32,12 @@ int _write(int fd, void *buf, size_t count){
 extern char _end;
 static intptr_t brk = (intptr_t)&_end;
 void *_sbrk(intptr_t increment){
-  intptr_t before = brk;
-  intptr_t now = brk + increment;
-  if(_syscall_(SYS_brk, now, 0, 0) == 0) {
-    brk = now;
-    return (void*) before;
-  }
+  // intptr_t before = brk;
+  // intptr_t now = brk + increment;
+  // if(_syscall_(SYS_brk, now, 0, 0) == 0) {
+  //   brk = now;
+  //   return (void*) before;
+  // }
   return (void *)-1;
 }
 
