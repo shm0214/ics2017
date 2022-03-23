d8c957c331cfa0e3ea76833c3a86451be967b188
tracer-ics2017 <tracer@njuics.org>
2022-03-23 18:06:37 +0800

    >  compile 1911463 shm0214 Linux shm0214 4.15.0-171-generic #180-Ubuntu SMP Wed Mar 2 17:24:46 UTC 2022 i686 i686 i686 GNU/Linux  18:06:37 up  3:42,  1 user,  load average: 0.63, 0.59, 0.46 1f8db70074f9b3be8ad39c79df2c3551fc0b1d7c

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -8,8 +8,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.eflags);
   rtl_push(&cpu.CS);
   rtl_push(&cpu.eip);
-  rtl_li(&t0, vaddr_read(cpu.idtr.base + NO * 4, 2));
-  rtl_li(&t1, vaddr_read(cpu.idtr.base + NO * 4 + 6, 2));
+  rtl_li(&t0, vaddr_read(cpu.idtr.base + NO * 8, 2));
+  rtl_li(&t1, vaddr_read(cpu.idtr.base + NO * 8 + 6, 2));
   decoding.jmp_eip = (t0<<16)| (0xffff&t1);
   decoding.is_jmp = 1;
   
