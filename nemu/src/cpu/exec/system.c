f9bfdb3d8f1cd6be04e95a3d54b5ec52aaf8908b
tracer-ics2017 <tracer@njuics.org>
2022-03-23 18:09:02 +0800

    >  compile 1911463 shm0214 Linux shm0214 4.15.0-171-generic #180-Ubuntu SMP Wed Mar 2 17:24:46 UTC 2022 i686 i686 i686 GNU/Linux  18:09:02 up  3:44,  1 user,  load average: 0.72, 0.68, 0.52 6ac5b1229dddbfa628ad9b55754f0778ca88e167

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -8,7 +8,7 @@ void diff_test_skip_nemu();
 make_EHelper(lidt) {
   rtl_li(&t0, id_dest->val);
   cpu.idtr.limit = vaddr_read(t0, 2);
-  cpu.idtr.base = vaddr_read(t0, 2);
+  cpu.idtr.base = vaddr_read(t0 + 2, 4);
   if(decoding.is_operand_size_16)
     cpu.idtr.base &= 0xffffff;
   print_asm_template1(lidt);
