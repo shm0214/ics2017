d08619fb332318c9718c2b9af31d5f88dce05630
tracer-ics2017 <tracer@njuics.org>
2022-03-23 21:32:55 +0800

    >  run 1911463 shm0214 Linux shm0214 4.15.0-171-generic #180-Ubuntu SMP Wed Mar 2 17:24:46 UTC 2022 i686 i686 i686 GNU/Linux  21:32:55 up  7:08,  1 user,  load average: 0.16, 0.23, 0.25 8a9eb83fa28b0c7fc723ac3aa1e683683a514233

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,12 +4,15 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
 
   switch (a[0]) {
     case SYS_none:
       SYSCALL_ARG1(r) = 1;
       break;
-
+    case SYS_exit:
+      _halt(a[1]);
+      break;
     default: 
       panic("Unhandled syscall ID = %d", a[0]);
   }
