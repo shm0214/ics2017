9488a0b4b3dd30e4da8614d512f51411b8c851f0
tracer-ics2017 <tracer@njuics.org>
2022-04-21 23:25:17 +0800

    >  run 1911463 shm0214 Linux shm0214 4.15.0-175-generic #184-Ubuntu SMP Thu Mar 24 17:48:18 UTC 2022 i686 i686 i686 GNU/Linux  23:25:17 up  3:29,  1 user,  load average: 0.74, 0.78, 0.65 f8467ebc1cb8d50da0e421ef687d55ac460bd60

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,6 +6,7 @@ extern int fs_open(const char *pathname, int flags, int mode);
 extern ssize_t fs_read(int fd, void *buf, size_t len);
 extern size_t fs_filesz(int fd);
 extern int fs_close(int fd);
+void* new_page(void);
 
 #define DEFAULT_ENTRY ((void *)0x8048000)
 
@@ -14,7 +15,15 @@ uintptr_t loader(_Protect *as, const char *filename) {
   // ramdisk_read((void *)DEFAULT_ENTRY, 0, size);
   int fd = fs_open(filename, 0, 0);
   size_t size = fs_filesz(fd);
-  fs_read(fd, (void*)DEFAULT_ENTRY, size);
+  void* va = DEFAULT_ENTRY;
+  void* pa;
+  while (size > 0) {
+    pa = new_page();
+    _map(as, va, pa);
+    fs_read(fd, pa, 4096);
+    va += 4096;
+    size -= 4096;
+  }
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -69,7 +69,6 @@ void _switch(_Protect *p) {
 void _map(_Protect *p, void *va, void *pa) {
   PDE* pgdir = &((PDE*)(p->ptr))[PDX(va)];
   PTE* ptep;
-  printf("%d", pgdir);
   if (*pgdir & 0x1) {
     ptep = (PTE*)PTE_ADDR(*pgdir);
   } else {
