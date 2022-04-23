0d021e0e0bec94452e5cf59aa9dc0b5e3a40db92
tracer-ics2017 <tracer@njuics.org>
2022-04-23 23:31:09 +0800

    >  run 1911463 shm0214 Linux shm0214 4.15.0-175-generic #184-Ubuntu SMP Thu Mar 24 17:48:18 UTC 2022 i686 i686 i686 GNU/Linux  23:31:09 up 18:22,  1 user,  load average: 0.40, 0.55, 0.43 c665ab3b4671c359ab8fe931f7fc893bf25923

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -32,9 +32,10 @@ int main() {
   init_fs();
   // uint32_t entry = loader(NULL, "/bin/pal");
   // ((void (*)(void))entry)();
-  load_prog("/bin/pal");
-  load_prog("/bin/hello");
-  load_prog("/bin/videotest");
+  // load_prog("/bin/pal");
+  // load_prog("/bin/hello");
+  // load_prog("/bin/videotest");
+  load_prog("bin/events");
   _trap();
   panic("Should not reach here");
 }
