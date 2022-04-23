3b1f7f9501f2ab9d69d053e55c05cc5eb1fee660
tracer-ics2017 <tracer@njuics.org>
2022-04-23 22:37:09 +0800

    >  run 1911463 shm0214 Linux shm0214 4.15.0-175-generic #184-Ubuntu SMP Thu Mar 24 17:48:18 UTC 2022 i686 i686 i686 GNU/Linux  22:37:09 up 17:29,  1 user,  load average: 1.30, 0.62, 0.59 6c20598b925621d439c54325d6e4e659b4b38305

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,6 +8,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
       do_syscall(r);
+      return schedule(r);
       break;
     case _EVENT_TRAP:
       return schedule(r);
